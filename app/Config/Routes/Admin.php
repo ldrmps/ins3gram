850b5e3a8e7493113b321bb995ee68f90289d3fa
Léandre <leandre.maupas@gmail.com>
2025-09-02 18:28:18 +0200

    feat: CRUD brand (#16)
    Co-authored-by: l1e <l1e@localhost>

--- app/Config/Routes/Admin.php
+++ app/Config/Routes/Admin.php
@@ -24,4 +24,11 @@ $routes->group('admin', ['namespace' => 'App\Controllers\Admin', 'filter' => 'au
         $routes->get('(:num)', 'Recipe::edit/$1');
         $routes->get('new', 'Recipe::create');
     });
+
+    $routes->group('brand', function ($routes) {
+        $routes->get('/', 'Brand::index');
+        $routes->post('update', 'Brand::update');
+        $routes->post('insert', 'Brand::insert');
+        $routes->post('delete', 'Brand::delete');
+    });
 });
