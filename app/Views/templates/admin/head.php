b57e097374368f0087ebda2125c5778df5e15de4
Léandre <leandre.maupas@gmail.com>
2025-09-11 13:20:58 +0200

    feat: recipe step (#28)
    Co-authored-by: l1e <l1e@localhost>

--- app/Views/templates/admin/head.php
+++ app/Views/templates/admin/head.php
@@ -38,6 +38,8 @@
 
     <!-- Javascript -->
     <script src="<?= base_url('/js/jquery-3.7.1.min.js') ?>"></script>
+    <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js" defer></script>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.4/dist/chart.umd.min.js"></script>
     <script src="<?= base_url('/js/config.js') ?>"></script>
     <script src="<?= base_url('/vendors/@coreui/coreui/js/coreui.bundle.min.js') ?>"></script>
@@ -52,7 +54,6 @@
     <script src="<?= base_url('/js/toastr.min.js') ?>"></script>
     <script src="<?= base_url('/js/tinymce/tinymce.min.js') ?>"></script>
     <script src="<?= base_url('/js/main.js') ?>"></script>
-
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js" integrity="sha512-GWzVrcGlo0TxTRvz9ttioyYJ+Wwk9Ck0G81D+eO63BaqHaJ3YZX9wuqjwgfcV/MrB2PhaVX9DkYVhbFpStnqpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
