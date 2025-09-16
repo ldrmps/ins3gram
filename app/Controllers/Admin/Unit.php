2e6c775d70eb765743ae27d2e10784c4617a318a
l1e <l1e@localhost>
2025-09-17 01:53:44 +0200

    feat : units crud menu + recipe picture upload

--- app/Controllers/Admin/Unit.php
+++ app/Controllers/Admin/Unit.php
@@ -7,26 +7,67 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class Unit extends BaseController
 {
+    public function index() {
+        helper('form');
+        return $this->view('admin/unit');
+    }
+    public function insert() {
+        $um = Model('UnitModel');
+        $data=$this->request->getPost();
+        if($um->insert($data)) {
+            $this->success ("Unité crée avec succès !");
+        } else {
+            foreach ($um->errors() as $key => $error) {
+                $this->error($error);
+            }
+        }
+        return $this->redirect('admin/unit');
+    }
+    public function update() {
+        $um = Model('UnitModel');
+        $data=$this->request->getPost();
+        $id=$data['id'];
+        unset($data['id']);
+        if ($um->update($id, $data)) {
+            return $this->response->setJSON([
+                'success' => true,
+                'message' => 'Unité modifiée avec succès !',
+            ]);
+        } else {
+            return $this->response->setJSON([
+                'success' => false,
+                'message' => $um->errors()
+            ]);
+        }
+    }
+    public function delete() {
+        $um = Model('UnitModel');
+        $id = $this->request->getPost('id');
+        if ($um->delete($id)) {
+            return $this->response->setJSON([
+                'success' => true,
+                'message' => 'Unité supprimée !',
+            ]);
+        } else {
+            return $this->response->setJSON([
+                'success' => false,
+                'message' => $um->errors()
+            ]);
+        }
+    }
     public function search()
     {
         $request = $this->request;
-
-        // Vérification AJAX
         if (!$request->isAJAX()) {
             return $this->response->setJSON(['error' => 'Requête non autorisée']);
         }
 
         $um = Model('UnitModel');
-
-        // Paramètres de recherche
         $search = $request->getGet('search') ?? '';
         $page = (int)($request->getGet('page') ?? 1);
         $limit = 20;
 
-        // Utilisation de la méthode du Model (via le trait)
         $result = $um->quickSearchForSelect2($search, $page, $limit);
-
-        // Réponse JSON
         return $this->response->setJSON($result);
     }
 }
