11c28758aefe2689573c2df42d14a39287ede8eb
l1e <l1e@localhost>
2025-09-19 09:19:10 +0200

    feat : front update V2

--- app/Config/Routes/Site.php
+++ app/Config/Routes/Site.php
@@ -6,5 +6,9 @@ $routes->get('/sign-in', 'Auth::signIn');
 $routes->post('/auth/login', 'Auth::login');
 $routes->get('/auth/logout', 'Auth::logout');
 
+$routes->group('recette', function ($routes) {
+    $routes->get('/', 'Recipe::index');
+    $routes->get('(:any)', 'Recipe::show/$1');
+});
 //dataTable
 $routes->post('/datatable/searchdatatable', 'DataTable::searchdatatable');
---
+++ app/Views/front/recipe/index.php
@@ -0,0 +1,2 @@
+<?php
+    echo "coucou de l'index de recette";
