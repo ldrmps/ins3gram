087f076ffc5497ac85a753f5bd8e98536e7f74d5
l1e <l1e@localhost>
2025-09-19 11:36:54 +0200

    feat : front update V3

--- app/Models/RecipeModel.php
+++ app/Models/RecipeModel.php
@@ -14,7 +14,7 @@ class RecipeModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = true;
     protected $protectFields    = true;
-    protected $allowedFields    = ['name', 'alcool','id_user','description'];
+    protected $allowedFields    = ['name','slug', 'alcool','id_user','description'];
     // Dates
     protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
@@ -58,6 +58,49 @@ class RecipeModel extends Model
         ],
     ];
 
+    /**
+     * Récupère depuis la base de données une recette avec tous ses éléments associés (étapes, ingrédients, mots clés, utilisateur, images ...)
+     * @param $id null Récupère depuis un ID (mettre null pour récupérer via le slug)
+     * @param $slug null Récupère depuis un slug (l'ID doit être à null sinon il est prioritaire)
+     * @return array Tableau contenant toutes les informations de notre recette
+     */
+    public function getFullRecipe($id = null, $slug = null) {
+        if($id != null) {
+            $recipe = $this->withDeleted()->find($id);
+        } elseif($slug != null) {
+            $recipe = $this->where('slug', $slug)->withDeleted()->first();
+        } else {
+            return [];
+        }
+        $id_recipe = $recipe['id'];
+        //Récupération de l'utilisateur qui à créé la recette (même s'il est désactivé)
+        $user = Model('UserModel')->withDeleted()->find($recipe['id_user']);
+        unset($recipe['id_user']);
+        $recipe['user'] = $user; //on ajoute à notre tableau de recipe
+        //Récupération des ingrédients (via la table Quantity)
+        $ingredients = Model('QuantityModel')->getQuantityByRecipe($id_recipe);
+        $recipe['ingredients'] = $ingredients; //on ajoute au tableau recipe
+        //Gestion pour le cas d'un ID (notamment pour l'édition en BO)
+        if($id != null) {
+            //Récupération des mots clés associés à notre recette
+            $recipe_tags = Model('TagRecipeModel')->where('id_recipe', $id_recipe)->findAll();
+            //Création d'un tableau à une dimension pour utiliser in_array (directement dans notre tableau recipe)
+            foreach ($recipe_tags as $recipe_tag) {
+                $recipe['tags'][] = $recipe_tag['id_tag'];
+            }
+        } else { //Cas d'un SLUG (notamment pour l'affichage en FO)
+            $recipe['tags'] = Model('TagRecipeModel')->join('tag','tag_recipe.id_tag = tag.id')->where('id_recipe', $id_recipe)->findAll();
+        }
+        $mediamodel = Model('MediaModel');
+        //Récupération de l'image principale et stocker dans le tableau recipe
+        $recipe['mea'] = $mediamodel->where('entity_id', $id_recipe)->where('entity_type', 'recipe_mea')->first();
+        //Récupération des images de la recette et stocker dans le tableau recipe
+        $recipe['images'] = $mediamodel->where('entity_id', $id_recipe)->where('entity_type', 'recipe')->findAll();
+        //Récupération des étapes de la recette
+        $steps = Model('StepModel')->where('id_recipe', $id_recipe)->orderBy('order', 'ASC')->findAll();
+        $recipe['steps'] = $steps; //on ajoute à notre recette
+        return $recipe;
+    }
     public function reactive(int $id): bool
     {
         return $this->builder()
--- app/Views/admin/recipe/index.php
+++ app/Views/admin/recipe/index.php
@@ -85,6 +85,10 @@
                                    class="btn btn-sm btn-warning" title="Modifier">
                                     <i class="fas fa-edit"></i>
                                 </a>
+                                 <a href="<?= base_url('/recette/') ?>${row.slug}"
+                                   class="btn btn-sm btn-primary" target="_blank" title="Voir la recette">
+                                   <i class="fas fa-eye"></i>
+                                </a>
                             </div>
                         `;
                     }
